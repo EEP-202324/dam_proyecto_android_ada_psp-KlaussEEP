e89129f4ad473b28e06dd42ac3641e1fa3fa2019
KlaussEEP <145350573+KlaussEEP@users.noreply.github.com>
2024-05-07 19:01:37 +0200

    List Tests creados
    Los test sobre el array de interinos

--- Eclipse/interns/src/test/java/com/epp/interino/interns/InternsJsonTest.java
+++ Eclipse/interns/src/test/java/com/epp/interino/interns/InternsJsonTest.java
@@ -5,8 +5,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import org.assertj.core.util.Arrays;
 
 import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
@@ -18,32 +20,50 @@ public class InternsJsonTest {
 	@Autowired
     private JacksonTester<Interns> json;
 	
+	@Autowired
+    private JacksonTester<Interns[]> jsonList;
+	
+	private Interns[] interns;
+
+    @BeforeEach
+    void setUp() {
+    	interns = Arrays.array(
+                new Interns(1, "Pepe", "Perez", 1000.00),
+                new Interns(2, "Juan", "Lama", 1200.00),
+                new Interns(3, "Sancho", "Ramos", 1100.00));
+    }
+	
 	@Test
 	void internsSerializationTest() throws IOException {
-		Interns interns = new Interns(1, "Pepe", "Perez", 1000.00);
+		Interns intern = new Interns(1, "Pepe", "Perez", 1000.00);
 		
-		InputStream isTmp = InternsJsonTest.class.getClassLoader().getResourceAsStream("com/epp/interino/interns/expected.json");
+		InputStream isTmpA = InternsJsonTest.class.getClassLoader().getResourceAsStream("com/epp/interino/interns/expected.json");
+		
+        String a = IOUtils.toString(isTmpA, StandardCharsets.UTF_8);
+        
+        assertThat(json.write(intern)).isStrictlyEqualToJson(a);
 
-        String a = IOUtils.toString(isTmp, StandardCharsets.UTF_8);
-		  
-        assertThat(json.write(interns)).isStrictlyEqualToJson(a);
-
-        assertThat(json.write(interns)).hasJsonPathNumberValue("@.id");
-        assertThat(json.write(interns)).extractingJsonPathNumberValue("@.id")
+        assertThat(json.write(intern)).hasJsonPathNumberValue("@.id");
+        assertThat(json.write(intern)).extractingJsonPathNumberValue("@.id")
         	.isEqualTo(1);
         
-        assertThat(json.write(interns)).hasJsonPathStringValue("@.name");
-        assertThat(json.write(interns)).extractingJsonPathStringValue("@.name")
+        assertThat(json.write(intern)).hasJsonPathStringValue("@.name");
+        assertThat(json.write(intern)).extractingJsonPathStringValue("@.name")
         	.isEqualTo("Pepe");
         
-        assertThat(json.write(interns)).hasJsonPathStringValue("@.surname");
-        assertThat(json.write(interns)).extractingJsonPathStringValue("@.surname")
+        assertThat(json.write(intern)).hasJsonPathStringValue("@.surname");
+        assertThat(json.write(intern)).extractingJsonPathStringValue("@.surname")
         	.isEqualTo("Perez");
         
-        assertThat(json.write(interns)).hasJsonPathNumberValue("@.amount");
-        assertThat(json.write(interns)).extractingJsonPathNumberValue("@.amount")
+        assertThat(json.write(intern)).hasJsonPathNumberValue("@.amount");
+        assertThat(json.write(intern)).extractingJsonPathNumberValue("@.amount")
              .isEqualTo(1000.00);
     }
+	
+	@Test
+	void internsListSerializationTest() throws IOException {
+		 assertThat(jsonList.write(interns)).isStrictlyEqualToJson("list.json");
+	}
 	
 	@Test
 	void internsDeserializationTest() throws IOException {
@@ -62,5 +82,17 @@ public class InternsJsonTest {
 	   assertThat(json.parseObject(expected).surname()).isEqualTo("Lopez");
 	   assertThat(json.parseObject(expected).amount()).isEqualTo(67.89);
 	}
-}
 
+	@Test
+	void internsListDeserializationTest() throws IOException {
+		String expected="""
+		        [
+					{ "id":1, "name":"Pepe", "surname":"Perez", "amount":1000.00 },
+					{ "id":2, "name":"Juan", "surname":"Lama", "amount":1200.00 },
+					{ "id":3, "name":"Sancho", "surname":"Ramos", "amount":1100.00 }
+				]
+		         """;
+		   assertThat(jsonList.parse(expected)).isEqualTo(interns);
+	}
+	
+}
--- Eclipse/interns/src/test/resources/data.sql
+++ Eclipse/interns/src/test/resources/data.sql
@@ -1,1 +1,3 @@
 INSERT INTO interns (ID, NAME, SURNAME, AMOUNT) VALUES (1, 'Pepe', 'Perez', 1000.00);
+INSERT INTO interns (ID, NAME, SURNAME, AMOUNT) VALUES (2, 'Juan', 'Lama', 1200.00);
+INSERT INTO interns (ID, NAME, SURNAME, AMOUNT) VALUES (3, 'Sancho', 'Ramos', 1100.00);
